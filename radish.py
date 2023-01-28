25d4643f25c4d0d90cb624a0cfc649760d9a1fda
Luke Wang <105933405+LukeWang2@users.noreply.github.com>
2023-01-28 14:15:41 -0500

    similarity checking

--- radish.py
+++ radish.py
@@ -1,5 +1,6 @@
 import cohere
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,6 +11,22 @@ def test():
 
 
 co = cohere.Client("syQmRBFrWWq3tpYnYwLz0TZuqIxjhitXqUbmWR5J")
-prompt = input("Test").strip()
-response = co.generate(prompt=prompt, model="xlarge", temperature=0.6, max_tokens=128)
-print(response.generations[0].text)
+
+
+def textGeneration():
+    prompt = input("Test ").strip()
+    response = co.generate(
+        prompt=prompt, model="xlarge", temperature=0.6, max_tokens=128
+    )
+    print(response.generations[0].text)
+
+
+def checkSimilary(phrase1, phrase2):
+    phrases = [phrase1, phrase2]
+    p1, p2 = co.embed(phrases).embeddings
+    return calculate_similarity(p1, p2)
+
+
+def calculate_similarity(a, b):
+    # compare thems
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
