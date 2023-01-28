cb1a252be75b8c446f3b396f2c9008dc01b79138
Luke Wang <105933405+LukeWang2@users.noreply.github.com>
2023-01-28 14:47:58 -0500

    add cohere prompt

--- radish.py
+++ radish.py
@@ -14,14 +14,19 @@ co = cohere.Client("syQmRBFrWWq3tpYnYwLz0TZuqIxjhitXqUbmWR5J")
 
 
 def textGeneration():
-    prompt = input("Test ").strip()
+    favRestaurants = input(
+        "What restaurants do you like? (input a comma separated list) "
+    ).strip()
+    favFoods = input("What are your favourite foods? (input a comma separated list) ")
+    prompt = f"This is a conversation between someone looking for food recommendations and their friend who will recommend similar food to the ones that the other person already likes. Friend looking for food recommendations: 'I like to eat at {favRestaurants} and my favourite foods are {favFoods} what food do you think I should try? Friend who will recommend food: I thnk you should try these dishes:"
+
     response = co.generate(
-        prompt=prompt, model="xlarge", temperature=0.6, max_tokens=128
+        prompt=prompt, model="xlarge", temperature=0.6, max_tokens=100
     )
     print(response.generations[0].text)
 
 
-def calculate_similarity(a, b):
+def calculateSimilarity(a, b):
     # compare themes
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
@@ -29,4 +34,4 @@ def calculate_similarity(a, b):
 def checkSimilary(phrase1, phrase2):
     phrases = [phrase1, phrase2]
     p1, p2 = co.embed(phrases).embeddings
-    return calculate_similarity(p1, p2)
+    return calculateSimilarity(p1, p2)
