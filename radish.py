b85f0d363300a954eb1e844aca33afbafe4f250b
Luke Wang <105933405+LukeWang2@users.noreply.github.com>
2023-01-28 13:47:05 -0500

    add something

--- radish.py
+++ radish.py
@@ -1,3 +1,6 @@
 import cohere
 
 co = cohere.Client("syQmRBFrWWq3tpYnYwLz0TZuqIxjhitXqUbmWR5J")
+prompt = input("Test").strip()
+response = co.generate(prompt=prompt, model="xlarge", temperature=0.6, max_tokens=128)
+print(response.generations[0].text)
