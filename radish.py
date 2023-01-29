ebdbbb12c949bf55390f01839a750e14afbb98c8
Luke Wang <105933405+LukeWang2@users.noreply.github.com>
2023-01-28 22:31:28 -0500

    dynamodb

--- radish.py
+++ radish.py
@@ -1,15 +1,48 @@
 import cohere
 import numpy as np
+import boto3
+from boto3.dynamodb.conditions import Key
 
+AWS_ACCESS_KEY_ID = "AKIA4EX46IN5ZBZ3VRPW"
+AWS_SECRET_ACCESS_KEY = "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"
+REGION_NAME = "ca-central-1"
+
+# client is an interface that interacts with AWS
+db = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME,
+)
+table = db.Table("user_interests")
 
 co = cohere.Client("syQmRBFrWWq3tpYnYwLz0TZuqIxjhitXqUbmWR5J")
 
 
-def textGeneration():
-    favRestaurants = input(
-        "What restaurants do you like? (input a comma separated list) "
-    ).strip()
-    favFoods = input("What are your favourite foods? (input a comma separated list) ")
+def registerUser(username, password):
+    try:
+        table.put_item(
+            Item={"user": username, "pass": password, "interests": ""},
+            ConditionExpression=f"attribute_not_exists(Username)",
+        )
+    except:
+        print("This username taken.")
+
+
+def storeInterests(username, interests):
+    table.update_item(
+        Key={"user": username},
+        UpdateExpression="SET interests = list_append(interests, :val)",
+        ExpressionAttributeValues={":val": [interests]},
+    )
+
+
+def getInterests(username):
+    response = table.get_item(Key={"user": username})
+    return str(response["Item"]["interests"]).replace("[", "").replace("]", "")
+
+
+def generateFoodRecommendations(favRestaurants, favFoods):
     prompt = f"""This is a conversation between someone looking for food 
     recommendations and their friend who will recommend similar food to the 
     ones that the other person already likes. Friend looking for food 
@@ -20,7 +53,32 @@ def textGeneration():
     response = co.generate(
         prompt=prompt, model="xlarge", temperature=0.6, max_tokens=50
     )
-    print(response.generations[0].text)
+    return response.generations[0].text
+
+
+def generateRestaurantRecommendations(favRestaurants, favFoods):
+    prompt = f"""This is a conversation between someone looking for food 
+    recommendations and their friend who will recommend restaurants similar to 
+    the ones that the other person already likes. Friend looking for food 
+    recommendations: 'I like to eat at {favRestaurants} and my favourite foods 
+    are {favFoods} what restaurants do you think I should try? Friend who will 
+    recommend a restaurants: I think you should try these restaurants:"""
+
+    response = co.generate(
+        prompt=prompt, model="xlarge", temperature=0.6, max_tokens=50
+    )
+    return response.generations[0].text
+
+
+def generateRecommendation(type):
+    favRestaurants = input(
+        "What restaurants do you like? (input a comma separated list) "
+    ).strip()
+    favFoods = input("What are your favourite foods? (input a comma separated list) ")
+    if type == "food":
+        return generateFoodRecommendations(favRestaurants, favFoods)
+    elif type == "restaurant":
+        return generateRestaurantRecommendations(favRestaurants, favFoods)
 
 
 def calculateSimilarity(a, b):
